dbd84143b86b16f0826ebd28772c8579abeb30cd
Melotti <matheus.melotti@gmail.com>
2023-08-31 09:51:11 -0300

    :recycle: refactor: Modificação no sistema de print dos blocos da casa

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Agente:
     def __init__(self):
         self.localizacao = ["A", 0]
@@ -14,14 +11,14 @@ class Agente:
         print(f"ANDEI? {'SIM' if self.andar else 'NÃO'}")
         print(f"POSICOES LIMPAS? {self.posicoes_limpas}")
 
-    def __verifica_acao__(self, casa):
-        if casa.sujo[self.localizacao[0]]:
-            casa = Agente.__limpar__(self=self, casa=casa)
+    def __verifica_acao__(self, casa_limpador):
+        if casa_limpador.sujo[self.localizacao[0]]:
+            casa_limpador = Agente.__limpar__(self=self, casa_limpador=casa_limpador)
             Agente.__mostra_acao__(self=self)
         else:
             Agente.__mudar_posicao__(self=self)
             Agente.__mostra_acao__(self=self)
-        return casa
+        return casa_limpador
 
     def __mudar_posicao__(self):
         self.limpar = False
@@ -31,12 +28,12 @@ class Agente:
         elif self.andar:
             self.localizacao[0], self.localizacao[1] = "A", 0
 
-    def __limpar__(self, casa):
+    def __limpar__(self, casa_limpador):
         self.limpar = True
         self.andar = False
         self.posicoes_limpas.append(self.localizacao[0])
-        casa.sujo[self.localizacao[0]] = False
-        return casa
+        casa_limpador.sujo[self.localizacao[0]] = False
+        return casa_limpador
 
 
 class Ambiente:
@@ -44,23 +41,23 @@ class Ambiente:
         self.blocos = {"A": 0, "B": 1}
         self.sujo = {"A": False, "B": False}
 
-    def __mostra_ambiente__(self, robo):
-        print(f"{robo}: SITUAÇÃO DA CASA - {self.sujo}")
+    def __mostra_ambiente__(self, modulo):
+        print(f"{modulo}: SITUAÇÃO DA CASA - {self.sujo}")
 
 
 class Sujador:
     @staticmethod
-    def __sujar_bloco__(casa, robo):
-        if not casa.sujo["A"] and not casa.sujo["B"]:
-            if robo.localizacao[0] == "A":
-                casa.sujo["B"] = True
+    def __sujar_bloco__(casa_sujador, limpador):
+        if not casa_sujador.sujo["A"] and not casa_sujador.sujo["B"]:
+            if limpador.localizacao[0] == "A":
+                casa_sujador.sujo["B"] = True
             else:
-                casa.sujo["A"] = True
-        elif casa.sujo["A"] and not casa.sujo["B"] and robo.localizacao[0] == "A":
-            casa.sujo["B"] = True
-        elif not casa.sujo["A"] and casa.sujo["B"] and robo.localizacao[0] == "B":
-            casa.sujo["A"] = True
-        return casa
+                casa_sujador.sujo["A"] = True
+        elif casa_sujador.sujo["A"] and not casa_sujador.sujo["B"] and limpador.localizacao[0] == "A":
+            casa_sujador.sujo["B"] = True
+        elif not casa_sujador.sujo["A"] and casa_sujador.sujo["B"] and limpador.localizacao[0] == "B":
+            casa_sujador.sujo["A"] = True
+        return casa_sujador
 
 
 if __name__ == "__main__":
@@ -68,9 +65,10 @@ if __name__ == "__main__":
     sujador = Sujador()
     robo = Agente()
 
-    #while True:
+    # while True:
     for i in range(10):
-        casa = sujador.__sujar_bloco__(casa=casa, robo=robo)
-        print("SUJADOR: ", casa.sujo)
-        casa = robo.__verifica_acao__(casa=casa)
-        print("LIMPADOR: ", casa.sujo, "\n")
+        casa = sujador.__sujar_bloco__(casa_sujador=casa, limpador=robo)
+        casa.__mostra_ambiente__(modulo="SUJADOR")
+        casa = robo.__verifica_acao__(casa_limpador=casa)
+        casa.__mostra_ambiente__(modulo="LIMPADOR")
+        print()
